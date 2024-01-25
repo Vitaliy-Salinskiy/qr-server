3b8ab5e9552663c6ab8eedc2ed7cfa33a2d2f574
Vitaliy <salinskiyvitaliy@gmail.com>
2024-01-25 10:16:15 +0100

    documentation v1.0.0

--- src/routes/request.routes.js
+++ src/routes/request.routes.js
@@ -4,9 +4,105 @@ import { RequestController } from "../controllers/request.controller.js";
 const router = Router();
 const requestController = new RequestController();
 
+/**
+ * @swagger
+ *  /requests:
+ *   get:
+ *    tags: [Requests]
+ *    summary: Get all requests
+ *    responses:
+ *     200: 
+ *      description: List of requests
+ *      content: 
+ *       application/json:
+ *        schema:
+ *         type: array
+ *         items:
+ *          $ref: '#/components/schemas/Request'
+ */
+
 router.get('/', requestController.getRequests);
+
+/**
+ * @swagger
+ *  /products/{id}/allow:
+ *   post:
+ *    tags: [Requests]
+ *    summary: Allow request
+ *    parameters:
+ *     - in: path
+ *       name: id
+ *       description: MongoDB unique identifier of the request
+ *       required: true
+ *       schema:
+ *        type: string
+ *       example: '65b09a2afd9a1b910c069814'
+ *    responses:
+ *     200:
+ *      description: Request allowed
+ *      content:
+ *       application/json:
+ *        schema:
+ *         $ref: '#/components/schemas/Request'
+ */
+
 router.post('/:id/allow', requestController.allowRequest);
+
+/**
+ * @swagger
+ *  /products/{id}/deny:
+ *   post:
+ *    tags: [Requests]
+ *    summary: Deny request
+ *    parameters:
+ *     - in: path
+ *       name: id
+ *       description: MongoDB unique identifier of the request
+ *       example: '65b09a2afd9a1b910c069814'
+ *       required: true
+ *       schema:
+ *        type: string
+ *    responses:
+ *     200:
+ *      description: Request allowed
+ *      content:
+ *       application/json:
+ *        schema:
+ *         $ref: '#/components/schemas/Request'
+ */
+
 router.post('/:id/deny', requestController.denyRequest);
+
+/**
+ * @swagger
+ *  /users/{userId}/{productId}:
+ *   post:
+ *    tags: [Requests]
+ *    summary: Add request
+ *    parameters:
+ *     - in: path
+ *       name: userId
+ *       description: MongoDB unique identifier of the user
+ *       required: true
+ *       schema:
+ *        type: string
+ *       example: '65b09a2afd9a1b910c069814'
+ *     - in: path
+ *       name: productId
+ *       description: MongoDB unique identifier of the product
+ *       required: true
+ *       schema:
+ *        type: string
+ *       example: '65b09a2afd9a1b910c069814'
+ *    responses:
+ *     200: 
+ *      description: Request object
+ *      content:
+ *       application/json:
+ *        schema:
+ *         $ref: '#/components/schemas/Request'
+ */
+
 router.post('/:userId/:productId', requestController.addRequest);
 
 export default router;
--- src/schemas/request.schema.js
+++ src/schemas/request.schema.js
@@ -13,25 +13,31 @@ import { Schema, model } from "mongoose";
  *      _id: 
  *       type: string
  *       description: MongoDB unique identifier
+ *       example: 65b09a2afd9a1b910c069814
  *      userId:
  *       type: string
  *       description: MongoDB unique identifier of the user
+ *       example: 9c46814aaf58f43eb1ad1bbc94c63e81
  *      productId:
  *       type: string
  *       description: MongoDB unique identifier of the product
+ *       example: 9c46814aaf58f43eb1ad1bbc94c63e81
  *      status: 
  *       type: string
  *       enum: [pending, allowed, denied]
  *       default: pending
  *       description: Status of the request
+ *       example: pending
  *      createdAt:
  *       type: string
  *       format: date-time
  *       description: Date of the request creation
+ *       example: 2024-01-24T05:04:12.755+00:00
  *      updatedAt:
  *       type: string
  *       format: date-time
  *       description: Date of the request update
+ *       example: 2024-01-24T05:05:12.755+00:00
  */
 
 const requestSchema = new Schema({
