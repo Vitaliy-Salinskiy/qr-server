63f82412237726644d31a2d7ffe673ed4b899dc3
Vitaliy <salinskiyvitaliy@gmail.com>
2024-01-25 08:00:25 +0100

    user routes documenting

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,11 +4,159 @@ import { UserController } from '../controllers/user.controller.js';
 const router = Router();
 const userController = new UserController();
 
+/**
+ * @swagger
+ *  /users:
+ *   get:
+ *    tags: [Users]
+ *    summary: Get all users
+ *    responses:
+ *     200:
+ *      description: List of users
+ *      content:
+ *       application/json:
+ *        schema:
+ *         type: array
+ *         items:
+ *          $ref: '#/components/schemas/User'
+ */
+
 router.get('/', userController.getUsers);
+
+/**
+ * @swagger
+ *  /users/scans:
+ *   get: 
+ *    tags: [Users]
+ *    summary: Get all scans
+ *    responses:
+ *     200:
+ *      description: Number of scans
+ *      content:
+ *       application/json:
+ *        schema:
+ *         type: number
+ *         description: Total number of scans
+ *         example: 121
+ */
+
 router.get('/scans', userController.getAllScans);
+
+/**
+ * @swagger
+ *  /users/{id}:
+ *   get: 
+ *    tags: [Users]
+ *    summary: Get user by id
+ *    parameters:
+ *     - in: path
+ *       name: id
+ *       schema:
+ *        type: string
+ *       required: true
+ *       description: MongoDB unique identifier of the user
+ *       example: '65b09a2afd9a1b910c069814'
+ *    responses:
+ *      200:
+ *       description: User object
+ *       content:
+ *        application/json:
+ *         schema:
+ *          $ref: '#/components/schemas/User'
+ */
+
 router.get('/:id', userController.getUser);
+
+/**
+ * @swagger
+ *  /users:
+ *   post:
+ *    tags: [Users]
+ *    summary: Create new user
+ *    requestBody:
+ *     required: true
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/User'
+ *       example:
+ *        id: '9c46814aaf58f43eb1ad1bbc94c63e81'
+ *        name: 'John'
+ *        surname: 'Doe'
+*    responses:
+ *     200:
+ *      description: User object
+ *      content:
+ *       application/json:
+ *        schema:
+ *         $ref: '#/components/schemas/User'
+ */
+
 router.post('/', userController.createUser);
+
+/**
+ * @swagger
+ *  /users/{id}:
+ *   put:
+ *    tags: [Users]
+ *    summary: increase timesScanned by 1 and add it to history
+ *    parameters:
+ *     - in: path
+ *       name: id
+ *       schema:
+ *        type: string
+ *       required: true
+ *       description: MongoDB unique identifier of the user
+ *       example: '65b09a2afd9a1b910c069814'
+ *    responses:
+ *     200:
+ *      description: User object
+ *      content:
+ *       application/json:
+ *        schema:
+ *         $ref: '#/components/schemas/User'
+ */
+
 router.put('/:id', userController.updateUser);
+
+/**
+ * @swagger
+ *  /users/{id}/credentials:
+ *   put:
+ *    tags: [Users]
+ *    summary: Add credentials (name, surname) to user
+ *    parameters:
+ *     - in: path
+ *       name: id
+ *       schema: 
+ *        type: string
+ *       required: true
+ *       description: MongoDB unique identifier of the user
+ *       example: '65b09a2afd9a1b910c069814'
+ *    requestBody:
+ *     required: true
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: object
+ *        properties:
+ *         name: 
+ *          type: string
+ *          description: Name of the user
+ *          example: 'John'
+ *         surname:
+ *          type: string
+ *          description: Surname of the user
+ *          example: 'Doe'
+ *    responses:
+ *     200:
+ *      description: User object
+ *      content:
+ *       application/json:
+ *        schema:
+ *         $ref: '#/components/schemas/User'
+ */
+
 router.put('/:id/credentials', userController.addCredentials);
 
 export default router;
--- src/schemas/user.schema.js
+++ src/schemas/user.schema.js
@@ -38,38 +38,55 @@ const ScanHistorySchema = new Schema({
  *     properties: 
  *       _id: 
  *         type: string
+ *         example: 65b09a2afd9a1b910c069814
  *         description: MongoDB unique identifier
  *       id: 
  *         type: string
  *         unique: true
  *         immutable: true
+ *         example: 9c46814aaf58f43eb1ad1bbc94c63e81
  *         description: Browser unique identifier
  *       lastScanned: 
  *         type: string
  *         format: date-time
+ *         example: 2024-01-24T05:04:12.755+00:00
  *         default: null
  *         description: Date of the last scan
  *       timesScanned:
  *         type: number
+ *         example: 32
  *         description: Total scans of the user
  *       scanHistory:
  *         type: array
  *         items:
  *           $ref: '#/components/schemas/ScanHistory'
  *         default: []
+ *         example: 
+ *           _id: 65b09a4cfd9a1b910c06981c
+ *           date: 2024-01-24T05:04:12.755+00:00
+ *           totalScans: 12
  *         description: Array of scan history
  *       name: 
  *        type: string
+ *        example: John
  *        description: User name
  *       surname:
  *        type: string
+ *        example: Doe
  *        description: User surname
  *       requests:
  *        type: array
  *        items:
- *          $ref: '#/components/schemas/Request'
+ *         $ref: '#/components/schemas/Request'
  *        default: []
  *        description: Array of requests of the user
+ *        example: 
+ *         _id: 65b09af1fd9a1b910c06986b
+ *         userId: 65b09a2afd9a1b910c069814
+ *         productId: 65afcd27775bea5debb91833
+ *         status: pending
+ *         createdAt: 2024-01-24T05:04:12.755+00:00
+ *         updatedAt: 2024-01-24T05:05:12.755+00:00
  */
 
 const UserSchema = new Schema({
