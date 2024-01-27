04a933bd48afcfcf05c1c43ffcf4c797f8fec313
Vitaliy <salinskiyvitaliy@gmail.com>
2024-01-27 10:36:37 +0100

    admin's tests

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -6,6 +6,9 @@ export class AdminController {
 	async getAdmin(req, res) {
 		try {
 			const admin = await Admin.findById(req.params.id);
+
+			if (!admin) return res.status(404).json({ message: "Admin not found" });
+
 			res.status(200).json(admin);
 		} catch (error) {
 			res.status(500).json({ message: error.message });
@@ -13,14 +16,22 @@ export class AdminController {
 	}
 
 	async createAdmin(req, res) {
+		console.log(req.body);
 		try {
+			const candidate = await Admin.findOne({ username: req.body.username });
+
+			if (candidate) {
+				return res.status(400).json({ message: "Username already exists" });
+			}
+
 			const salt = await bcrypt.genSalt(10);
 			const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
 
 			const admin = await new Admin({ username: req.body.username, password: hashedPassword }).save();
 			res.status(201).json(admin);
 		} catch (error) {
-			res.status(500).json({ message: error.message });
+			res.status(500).json({ message: error.message })
 		}
 	}
 
---
+++ test/controllers/admin.routes.test.js
@@ -0,0 +1,74 @@
+import request from 'supertest';
+import app from "../../src/index.js"
+import mongoose from 'mongoose';
+
+let server;
+
+beforeAll(() => {
+	server = app.listen(0, () => console.log(`Server running on port ${server.address().port}`));
+});
+
+afterAll(async () => {
+	server.close();
+	await mongoose.connection.close();
+});
+
+describe("Get /admins/:id", () => {
+
+	const adminId = "65b4ccc9d83c5906fceb689e";
+
+	describe("Get by id work alright", () => {
+
+		test("Admin Id should exist", async () => {
+			const response = await request(app).get(`/admins/${adminId}`).send()
+			const returnedAdminId = response.body._id;
+
+			expect(returnedAdminId).toBe(adminId)
+		})
+
+		test("Admin Id should not exist", async () => {
+			const nonExistingAdminId = "123456789012345678901234";
+			const response = await request(app).get(`/admins/${nonExistingAdminId}`).send()
+
+			expect(response.statusCode).toBe(404)
+		})
+
+		test("Should return a 200 status code", async () => {
+			const response = await request(app).get(`/admins/${adminId}`).send()
+
+			expect(response.statusCode).toBe(200)
+		})
+
+	});
+
+})
+
+describe("POST /admins", () => {
+
+	describe("Post admin work alright", () => {
+
+		test("Should return a 400 status code if admin with such username already exist", async () => {
+			const newAdmin = {
+				username: "admin",
+				password: "admin",
+			}
+
+			const response = await request(app).post("/admins").send(newAdmin)
+
+			expect(response.statusCode).toBe(400)
+		})
+
+		test("Should create a new admin", async () => {
+			const newAdmin = {
+				username: `test-admin-${Date.now()}-${Math.random().toFixed(5)}`,
+				password: "admin",
+			}
+
+			const response = await request(app).post("/admins").send(newAdmin)
+
+			expect(response.statusCode).toBe(201)
+		})
+
+	})
+
+})
